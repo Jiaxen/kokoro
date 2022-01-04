d7ca50599d6f220a4f3bb5bf2b155d0422745df2
jiaxen <jiaxen.lau@gmail.com>
2022-01-04 09:53:26 +0000

    Recreated flutter for google log in

--- android/app/src/main/java/com/jiaxen/kokoro/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.jiaxen.kokoro;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:kokoro/constants.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -42,9 +44,30 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                     color: kPrimaryFontColour,
                     fontSize: 18,
                     fontWeight: FontWeight.w500),
-              )
+              ),
+              TextButton(onPressed: signInWithGoogle, child: Text('Sign in with Google')),
             ]),
       ),
     );
   }
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 }
+
+
+
