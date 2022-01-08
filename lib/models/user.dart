577280e4d6702247a2beb65ac9aee71dd1d3179a
jiaxen <jiaxen.lau@gmail.com>
2022-01-08 18:30:48 +0000

    Recomment

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-// Naming this class AppUser to differentiate from firebase's User class
+/// Naming this class AppUser to differentiate from firebase's [User] class
 class AppUser {
   final String uid;
   String? email;
