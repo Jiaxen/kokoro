42ba499c0fcab4409411ab8f437d9881e5c239e4
jiaxen <jiaxen.lau@gmail.com>
2022-01-22 15:19:36 +0000

    Flutter code cleanup

--- lib/screens/edit_note_screen.dart
+++ lib/screens/edit_note_screen.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:kokoro/constants.dart';
 import 'package:kokoro/models/note.dart';
 import 'package:kokoro/services/note_services.dart';
-import 'package:provider/provider.dart';
-
-import '../utils.dart';
-import 'notes_screen.dart';
+import 'package:kokoro/utils.dart';
 
 class EditNoteScreen extends StatefulWidget {
-  Note note;
+  final Note note;
 
-  EditNoteScreen({Key? key, required this.note});
-
+  const EditNoteScreen({Key? key, required this.note}) : super(key: key);
 
   @override
   _EditNoteScreenState createState() => _EditNoteScreenState();
@@ -41,18 +36,18 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: AlertDialog(
-        insetPadding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-        contentPadding: EdgeInsets.symmetric(horizontal: 35, vertical: 50),
+        insetPadding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 35, vertical: 50),
         backgroundColor: kPrimaryBackgroundColour,
-        shape: RoundedRectangleBorder(
+        shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(25))),
-        content: Container(
+        content: SizedBox(
           width: double.maxFinite,
           child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 30),
+                const SizedBox(height: 30),
                 Text(
                   'A note of...',
                   style: TextStyle(
@@ -61,7 +56,7 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
                       fontStyle: FontStyle.italic,
                       fontSize: 20),
                 ),
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 DropdownButton<String>(
                   value: dropdownValue,
                   // icon: const Icon(Icons.a),
@@ -77,20 +72,21 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
                     color: kPrimaryAppColour,
                   ),
                   onChanged: (String? newValue) {
-                    print(newValue);
                     setState(() {
-                      widget.note.noteType = enumFromString(NoteType.values, newValue!)!;
+                      widget.note.noteType =
+                          enumFromString(NoteType.values, newValue!)!;
                       dropdownValue = newValue;
                     });
                   },
-                  items: noteTypes.map<DropdownMenuItem<String>>((String value) {
+                  items:
+                      noteTypes.map<DropdownMenuItem<String>>((String value) {
                     return DropdownMenuItem<String>(
                       value: value,
                       child: Text(value),
                     );
                   }).toList(),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 TextField(
                     controller: myController,
                     autofocus: true,
@@ -99,8 +95,8 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
                       focusedBorder: OutlineInputBorder(
                           borderSide: BorderSide(
                               color: kSecondaryAppColour, width: 2.0),
-                          borderRadius: BorderRadius.all(Radius.circular(20))),
-                      border: OutlineInputBorder(
+                          borderRadius: const BorderRadius.all(Radius.circular(20))),
+                      border: const OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(20)),
                       ),
                       hintText: 'Note for your meeting...',
@@ -109,72 +105,78 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
                     textCapitalization: TextCapitalization.sentences,
                     minLines: 5,
                     maxLines: 10),
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [TextButton(
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                          EdgeInsets.all(15)),
-                      backgroundColor:
-                      MaterialStateProperty.all<Color>(kWarningBackgroundColorLight),
-                      foregroundColor:
-                      MaterialStateProperty.all<Color>(kPrimaryTitleColour),
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                          )),
-                    ),
-                    onPressed: () {
-                      if (myController.text.isEmpty && widget.note.id == null){ // If it is empty and a new note
-                        Navigator.of(context).pop();
-                      } else {
-                        showDialog<String>(
-                          context: context,
-                          builder: (BuildContext context) => AlertDialog(
-                            title: const Text('Delete Note?'),
-                            content: const Text('Are you sure you want to delete this note?'),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(),
-                                child: const Text('No, go back!'),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  deleteNoteFromFireStore(widget.note);
-                                  Navigator.of(context).popUntil((route) => route.isFirst);
-                                },
-                                child: const Text('Yes please!'),
-                              ),
-                            ],
-                          ),
-                        );
-                      }
-                    },
-                    child: Text('Delete'),
-                  ),
+                  children: [
                     TextButton(
                       style: ButtonStyle(
                         padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                            EdgeInsets.all(15)),
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(kPrimaryAppColour),
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(kPrimaryTitleColour),
+                            const EdgeInsets.all(15)),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            kWarningBackgroundColorLight),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            kPrimaryTitleColour),
                         shape: MaterialStateProperty.all<OutlinedBorder>(
-                            RoundedRectangleBorder(
+                            const RoundedRectangleBorder(
                           borderRadius: BorderRadius.all(Radius.circular(20.0)),
                         )),
                       ),
                       onPressed: () {
-                          if (myController.text.isNotEmpty){
-                            widget.note.content = myController.text;
-                            widget.note.lastModifiedTime = DateTime.now();
-                            saveNoteToFireStore(widget.note, widget.note.groupId);
-                          }
+                        if (myController.text.isEmpty &&
+                            widget.note.id == null) {
+                          // If it is empty and a new note
+                          Navigator.of(context).pop();
+                        } else {
+                          showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                              title: const Text('Delete Note?'),
+                              content: const Text(
+                                  'Are you sure you want to delete this note?'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () => Navigator.of(context).pop(),
+                                  child: const Text('No, go back!'),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    deleteNoteFromFireStore(widget.note);
+                                    FocusScope.of(context).unfocus();
+                                    Navigator.of(context)
+                                        .popUntil((route) => route.isFirst);
+                                  },
+                                  child: const Text('Yes please!'),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      },
+                      child: const Text('Delete'),
+                    ),
+                    TextButton(
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                            const EdgeInsets.all(15)),
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(kPrimaryAppColour),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            kPrimaryTitleColour),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                            const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        )),
+                      ),
+                      onPressed: () {
+                        if (myController.text.isNotEmpty) {
+                          widget.note.content = myController.text;
+                          widget.note.lastModifiedTime = DateTime.now();
+                          saveNoteToFireStore(widget.note, widget.note.groupId);
+                        }
                         Navigator.pop(context);
                       },
-                      child: Text('Save note'),
+                      child: const Text('Save note'),
                     ),
                   ],
                 ),
