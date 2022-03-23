9a6f7b6821eb6d0d1ad83fd4ec8b4ac6e44e0fe6
Jiaxen <jiaxen.lau@gmail.com>
2022-03-23 20:32:53 +0000

    fix for rejecting room invite

--- lib/app/screens/onboarding/onboarding.dart
+++ lib/app/screens/onboarding/onboarding.dart
@@ -19,12 +19,17 @@ class OnboardingPage extends ConsumerStatefulWidget {
 }
 
 class _OnboardingPageState extends ConsumerState<OnboardingPage> {
-
   @override
   Widget build(BuildContext context) {
     final invitedRoomsAsyncValue = ref.watch(invitedRoomsProvider);
     return invitedRoomsAsyncValue.when(
-      data: (invitedRooms) => OnboardingStepper(invitedRooms: invitedRooms),
+      data: (invitedRooms) {
+        if (invitedRooms.isNotEmpty) {
+          return InvitedOnboardingStepper(invitedRooms: invitedRooms);
+        } else {
+          return OnboardingStepper();
+        }
+      },
       error: (_, __) => EmptyContent(
         title: 'Oops',
         message: 'Something went wrong',
@@ -34,13 +39,68 @@ class _OnboardingPageState extends ConsumerState<OnboardingPage> {
   }
 }
 
-class OnboardingStepper extends ConsumerStatefulWidget {
+class InvitedOnboardingStepper extends ConsumerStatefulWidget {
   final List<Room> invitedRooms;
+  int _index = 0;
+
+  InvitedOnboardingStepper({Key? key, required this.invitedRooms})
+      : super(key: key);
+
+  @override
+  ConsumerState<InvitedOnboardingStepper> createState() =>
+      _InvitedOnboardingStepperState();
+}
+
+class _InvitedOnboardingStepperState extends ConsumerState<InvitedOnboardingStepper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryAppColour,
+      body: Theme(
+        data: ThemeData(
+          colorScheme: Theme.of(context).colorScheme.copyWith(
+                primary: kPrimaryAppColour,
+              ),
+        ),
+        child: Stepper(
+          controlsBuilder: (_, __) {
+            return Container();
+          },
+          type: StepperType.horizontal,
+          currentStep: widget._index,
+          steps: <Step>[
+            Step(
+              isActive: widget._index >= 0,
+              title: const Text('Welcome'),
+              content: WelcomeStep(
+                nextStep: () {
+                  setState(() {
+                    widget._index += 1;
+                  });
+                },
+              ),
+            ),
+            Step(
+              isActive: widget._index >= 1,
+              title: const Text('Invitation'),
+              content: InvitedStep(
+                invitedRooms: widget.invitedRooms,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OnboardingStepper extends ConsumerStatefulWidget {
   int _index = 0;
   final partnerEmailController = TextEditingController();
   final roomNameController = TextEditingController();
 
-  OnboardingStepper({Key? key, required this.invitedRooms})
+  OnboardingStepper(
+      {Key? key})
       : super(key: key);
 
   @override
@@ -50,119 +110,79 @@ class OnboardingStepper extends ConsumerStatefulWidget {
 class _OnboardingStepperState extends ConsumerState<OnboardingStepper> {
   @override
   Widget build(BuildContext context) {
-    AppUser user = ref.watch(userProvider).value!;
-    if (widget.invitedRooms.isEmpty) {
-      return Scaffold(
-        backgroundColor: kPrimaryAppColour,
-        body: Theme(
-          data: ThemeData(
-            colorScheme: Theme.of(context).colorScheme.copyWith(
-                  primary: kPrimaryAppColour,
-                ),
-          ),
-          child: Stepper(
-            controlsBuilder: (_, __) {
-              return Container();
-            },
-            type: StepperType.horizontal,
-            currentStep: widget._index,
-            steps: <Step>[
-              Step(
-                isActive: widget._index >= 0,
-                title: const Text('Welcome'),
-                content: WelcomeStep(
-                  nextStep: () {
-                    setState(() {
-                      widget._index += 1;
-                    });
-                  },
-                ),
+    return Scaffold(
+      backgroundColor: kPrimaryAppColour,
+      body: Theme(
+        data: ThemeData(
+          colorScheme: Theme.of(context).colorScheme.copyWith(
+                primary: kPrimaryAppColour,
               ),
-              Step(
-                isActive: widget._index >= 1,
-                title: const Text('Partner'),
-                content: AddPartnerStep(
-                  emailController: widget.partnerEmailController,
-                  nextStep: () {
-                    setState(() {
-                      widget._index += 1;
-                    });
-                  },
-                  previousStep: () {
-                    setState(() {
-                      widget._index -= 1;
-                    });
-                  },
-                ),
-              ),
-              Step(
-                isActive: widget._index >= 2,
-                title: const Text('Room'),
-                content: AddRoomStep(
-                  roomController: widget.roomNameController,
-                  nextStep: () async {
-                    final database = ref.watch(databaseProvider)!;
-                    Room newRoom = Room(
-                        roomName: widget.roomNameController.text,
-                        members: [user.uid],
-                        invitedMembers: [
-                          widget.partnerEmailController.text.toLowerCase()
-                        ],
-                        createdTime: DateTime.now());
-                    DocumentReference newRoomDocument =
-                        await database.addRoom(newRoom);
-                    user.currentRoom = newRoomDocument.id;
-                    database.setUser(user);
-                  },
-                  previousStep: () {
-                    setState(() {
-                      widget._index -= 1;
-                    });
-                  },
-                ),
-              ),
-            ],
-          ),
         ),
-      );
-    } else {
-      return Scaffold(
-        backgroundColor: kPrimaryAppColour,
-        body: Theme(
-          data: ThemeData(
-            colorScheme: Theme.of(context).colorScheme.copyWith(
-                  primary: kPrimaryAppColour,
-                ),
-          ),
-          child: Stepper(
-            controlsBuilder: (_, __) {
-              return Container();
-            },
-            type: StepperType.horizontal,
-            currentStep: widget._index,
-            steps: <Step>[
-              Step(
-                isActive: widget._index >= 0,
-                title: const Text('Welcome'),
-                content: WelcomeStep(
-                  nextStep: () {
-                    setState(() {
-                      widget._index += 1;
-                    });
-                  },
-                ),
+        child: Stepper(
+          controlsBuilder: (_, __) {
+            return Container();
+          },
+          type: StepperType.horizontal,
+          currentStep: widget._index,
+          steps: <Step>[
+            Step(
+              isActive: widget._index >= 0,
+              title: const Text('Welcome'),
+              content: WelcomeStep(
+                nextStep: () {
+                  setState(() {
+                    widget._index += 1;
+                  });
+                },
               ),
-              Step(
-                isActive: widget._index >= 1,
-                title: const Text('Invitation'),
-                content: InvitedStep(
-                  invitedRooms: widget.invitedRooms,
-                ),
+            ),
+            Step(
+              isActive: widget._index >= 1,
+              title: const Text('Partner'),
+              content: AddPartnerStep(
+                emailController: widget.partnerEmailController,
+                nextStep: () {
+                  setState(() {
+                    widget._index += 1;
+                  });
+                },
+                previousStep: () {
+                  setState(() {
+                    widget._index -= 1;
+                  });
+                },
               ),
-            ],
-          ),
+            ),
+            Step(
+              isActive: widget._index >= 2,
+              title: const Text('Room'),
+              content: AddRoomStep(
+                roomController: widget.roomNameController,
+                nextStep: () async {
+                  final database = ref.watch(databaseProvider)!;
+                  final user = ref.watch(userProvider).value!;
+                  Room newRoom = Room(
+                      roomName: widget.roomNameController.text,
+                      members: [user.uid],
+                      invitedMembers: [
+                        widget.partnerEmailController.text.toLowerCase()
+                      ],
+                      createdTime: DateTime.now());
+                  DocumentReference newRoomDocument =
+                      await database.addRoom(newRoom);
+                  user.currentRoom = newRoomDocument.id;
+                  database.setUser(user);
+                },
+                previousStep: () {
+                  setState(() {
+                    widget._index -= 1;
+                  });
+                },
+              ),
+            ),
+          ],
         ),
-      );
-    }
+      ),
+    );
   }
 }
